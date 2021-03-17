e955500fbe2df56b9c1e80b8e16e2176ad7a1bb8
leigaoyi <leigaoyi@126.com>
2021-03-17 17:24:55 +0800

    Add files via upload

--- model_code/unet_v1.py
+++ model_code/unet_v1.py
@@ -1,319 +1,74 @@
-from keras.layers import Conv2DTranspose
-from keras.layers import UpSampling2D
-from keras.layers import Conv2D
-from keras.layers import BatchNormalization
-from keras.layers import Activation
-from keras.layers import Concatenate
-
-from keras.layers import Conv2D
-from keras.layers import Activation, Reshape
-from keras.models import Model
-
-from functools import wraps
 import numpy as np
-
-from keras.applications import DenseNet121, DenseNet169, DenseNet201
-from keras.applications import VGG16
-from keras.applications import VGG19
-
-
-backbones = {
-    "vgg16": VGG16,
-    "vgg19": VGG19,
-    "resnet18": ResNet18,
-    "resnet34": ResNet34,
-    "resnet50": ResNet50,
-    "resnet101": ResNet101,
-    "resnet152": ResNet152,
-    "resnext50": ResNeXt50,
-    "resnext101": ResNeXt101,
-    "inceptionresnetv2": InceptionResNetV2,
-    "inceptionv3": InceptionV3,
-    "densenet121": DenseNet121,
-    "densenet169": DenseNet169,
-    "densenet201": DenseNet201,
-
-}
-
-def get_backbone(name, *args, **kwargs):
-    return backbones[name](*args, **kwargs)
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
 
 
-def get_layer_number(model, layer_name):
-    """
-    Help find layer in Keras model by name
-    Args:
-        model: Keras `Model`
-        layer_name: str, name of layer
-    Returns:
-        index of layer
-    Raises:
-        ValueError: if model does not contains layer with such name
-    """
-    for i, l in enumerate(model.layers):
-        if l.name == layer_name:
-            return i
-    raise ValueError('No layer with name {} in  model {}.'.format(layer_name, model.name))
+def unet_v1(pretrained_weights=None, input_size=(256, 256, 1)):
+    inputs = Input(input_size)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
 
-def extract_outputs(model, layers, include_top=False):
-    """
-    Help extract intermediate layer outputs from model
-    Args:
-        model: Keras `Model`
-        layer: list of integers/str, list of layers indexes or names to extract output
-        include_top: bool, include final model layer output
-    Returns:
-        list of tensors (outputs)
-    """
-    layers_indexes = ([get_layer_number(model, l) if isinstance(l, str) else l
-                       for l in layers])
-    outputs = [model.layers[i].output for i in layers_indexes]
+    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        UpSampling2D(size=(2, 2))(drop5))
+    merge6 = concatenate([drop4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
-    if include_top:
-        outputs.insert(0, model.output)
+    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        UpSampling2D(size=(2, 2))(conv6))
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 
-    return outputs
+    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        UpSampling2D(size=(2, 2))(conv7))
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
+    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        UpSampling2D(size=(2, 2))(conv8))
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv10 = Conv2D(1, 1, activation=None)(conv9)
+    conv10 = Reshape((256,256))(conv10)
+    model = Model(inputs, conv10)
 
-def reverse(l):
-    """Reverse list"""
-    return list(reversed(l))
+    #model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
 
-
-# decorator for models aliases, to add doc string
-def add_docstring(doc_string=None):
-    def decorator(fn):
-        if fn.__doc__:
-            fn.__doc__ += doc_string
-        else:
-            fn.__doc__ = doc_string
-
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            return fn(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
-def recompile(model):
-    model.compile(model.optimizer, model.loss, model.metrics)
-
-
-def freeze_model(model):
-    for layer in model.layers:
-        layer.trainable = False
-    return
-
-
-def set_trainable(model):
-    for layer in model.layers:
-        layer.trainable = True
-    recompile(model)
-
-
-def to_tuple(x):
-    if isinstance(x, tuple):
-        if len(x) == 2:
-            return x
-    elif np.isscalar(x):
-        return (x, x)
-
-    raise ValueError('Value should be tuple of length 2 or int value, got "{}"'.format(x))
-
-
-def handle_block_names(stage):
-    conv_name = 'decoder_stage{}_conv'.format(stage)
-    bn_name = 'decoder_stage{}_bn'.format(stage)
-    relu_name = 'decoder_stage{}_relu'.format(stage)
-    up_name = 'decoder_stage{}_upsample'.format(stage)
-    return conv_name, bn_name, relu_name, up_name
-
-
-def ConvRelu(filters, kernel_size, use_batchnorm=False, conv_name='conv', bn_name='bn', relu_name='relu'):
-    def layer(x):
-        x = Conv2D(filters, kernel_size, padding="same", name=conv_name, use_bias=not(use_batchnorm))(x)
-        if use_batchnorm:
-            x = BatchNormalization(name=bn_name)(x)
-        x = Activation('relu', name=relu_name)(x)
-        return x
-    return layer
-
-
-def Upsample2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
-                     use_batchnorm=False, skip=None):
-
-    def layer(input_tensor):
-
-        conv_name, bn_name, relu_name, up_name = handle_block_names(stage)
-
-        x = UpSampling2D(size=upsample_rate, name=up_name)(input_tensor)
-
-        if skip is not None:
-            x = Concatenate()([x, skip])
-
-        x = ConvRelu(filters, kernel_size, use_batchnorm=use_batchnorm,
-                     conv_name=conv_name + '1', bn_name=bn_name + '1', relu_name=relu_name + '1')(x)
-
-        x = ConvRelu(filters, kernel_size, use_batchnorm=use_batchnorm,
-                     conv_name=conv_name + '2', bn_name=bn_name + '2', relu_name=relu_name + '2')(x)
-
-        return x
-    return layer
-
-
-def Transpose2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
-                      transpose_kernel_size=(4,4), use_batchnorm=False, skip=None):
-
-    def layer(input_tensor):
-
-        conv_name, bn_name, relu_name, up_name = handle_block_names(stage)
-
-        x = Conv2DTranspose(filters, transpose_kernel_size, strides=upsample_rate,
-                            padding='same', name=up_name, use_bias=not(use_batchnorm))(input_tensor)
-        if use_batchnorm:
-            x = BatchNormalization(name=bn_name+'1')(x)
-        x = Activation('relu', name=relu_name+'1')(x)
-
-        if skip is not None:
-            x = Concatenate()([x, skip])
-
-        x = ConvRelu(filters, kernel_size, use_batchnorm=use_batchnorm,
-                     conv_name=conv_name + '2', bn_name=bn_name + '2', relu_name=relu_name + '2')(x)
-
-        return x
-    return layer
-
-
-def build_unet(backbone, classes, skip_connection_layers,
-               decoder_filters=(256,128,64,32,16),
-               upsample_rates=(2,2,2,2,2),
-               n_upsample_blocks=5,
-               block_type='upsampling',
-               activation='sigmoid',
-               use_batchnorm=True):
-
-    input = backbone.input
-    x = backbone.output
-
-    if block_type == 'transpose':
-        up_block = Transpose2D_block
-    else:
-        up_block = Upsample2D_block
-
-    # convert layer names to indices
-    skip_connection_idx = ([get_layer_number(backbone, l) if isinstance(l, str) else l
-                               for l in skip_connection_layers])
-
-    for i in range(n_upsample_blocks):
-
-        # check if there is a skip connection
-        skip_connection = None
-        if i < len(skip_connection_idx):
-            skip_connection = backbone.layers[skip_connection_idx[i]].output
-
-        upsample_rate = to_tuple(upsample_rates[i])
-
-        x = up_block(decoder_filters[i], i, upsample_rate=upsample_rate,
-                     skip=skip_connection, use_batchnorm=use_batchnorm)(x)
-
-    x = Conv2D(classes, (3,3), padding='same', name='final_conv')(x)
-    x = Activation(activation, name=activation)(x)
-    x = Reshape((256,256))(x)
-
-    model = Model(input, x)
+    # # model.summary()
+    # if (pretrained_weights):
+    #     model.load_weights(pretrained_weights)
 
     return model
 
-
-DEFAULT_SKIP_CONNECTIONS = {
-    'vgg16':            ('block5_conv3', 'block4_conv3', 'block3_conv3', 'block2_conv2', 'block1_conv2'),
-    'vgg19':            ('block5_conv4', 'block4_conv4', 'block3_conv4', 'block2_conv2', 'block1_conv2'),
-    'resnet18':         ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'), # check 'bn_data'
-    'resnet34':         ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-    'resnet50':         ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-    'resnet101':        ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-    'resnet152':        ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-    'resnext50':        ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-    'resnext101':       ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
-    'inceptionv3':          (228, 86, 16, 9),
-    'inceptionresnetv2':    (594, 260, 16, 9),
-    'densenet121':          (311, 139, 51, 4),
-    'densenet169':          (367, 139, 51, 4),
-    'densenet201':          (479, 139, 51, 4),
-}
-
-
-def unet_v1(backbone_name='vgg16',
-         input_shape=(None, None, 3),
-         input_tensor=None,
-         encoder_weights='imagenet',
-         freeze_encoder=False,
-         skip_connections='default',
-         decoder_block_type='upsampling',
-         decoder_filters=(256,128,64,32,16),
-         decoder_use_batchnorm=True,
-         n_upsample_blocks=5,
-         upsample_rates=(2,2,2,2,2),
-         classes=1,
-         activation='sigmoid'):
-    """
-    Args:
-        backbone_name: (str) look at list of available backbones.
-        input_shape:  (tuple) dimensions of input data (H, W, C)
-        input_tensor: keras tensor
-        encoder_weights: one of `None` (random initialization),
-            'imagenet' (pre-training on ImageNet),
-            'dof' (pre-training on DoF)
-        freeze_encoder: (bool) Set encoder layers weights as non-trainable. Useful for fine-tuning
-        skip_connections: if 'default' is used take default skip connections,
-            else provide a list of layer numbers or names starting from top of model
-        decoder_block_type: (str) one of 'upsampling' and 'transpose' (look at blocks.py)
-        decoder_filters: (int) number of convolution layer filters in decoder blocks
-        decoder_use_batchnorm: (bool) if True add batch normalisation layer between `Conv2D` ad `Activation` layers
-        n_upsample_blocks: (int) a number of upsampling blocks
-        upsample_rates: (tuple of int) upsampling rates decoder blocks
-        classes: (int) a number of classes for output
-        activation: (str) one of keras activations for last model layer
-    Returns:
-        keras.models.Model instance
-    """
-
-
-
-    backbone = get_backbone(backbone_name,
-                            input_shape=input_shape,
-                            input_tensor=input_tensor,
-                            weights=encoder_weights,
-                            include_top=False)
-
-    if skip_connections == 'default':
-        skip_connections = DEFAULT_SKIP_CONNECTIONS[backbone_name]
-
-    model = build_unet(backbone,
-                       classes,
-                       skip_connections,
-                       decoder_filters=decoder_filters,
-                       block_type=decoder_block_type,
-                       activation=activation,
-                       n_upsample_blocks=n_upsample_blocks,
-                       upsample_rates=upsample_rates,
-                       use_batchnorm=decoder_use_batchnorm)
-
-    # lock encoder weights for fine-tuning
-    if freeze_encoder:
-        freeze_model(backbone)
-
-    model.name = 'u-{}'.format(backbone_name)
-
-    return model
-
-
-if __name__ == '__main__':
+if __name__=='__main__':
     # test unet
-    model = unet_v1(input_shape=(256, 256, 1), activation=None)
+    model = unet_v1()
     ## 143:333,
     model.summary()
 
